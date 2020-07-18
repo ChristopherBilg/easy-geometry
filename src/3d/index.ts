459f3215f087b42831ae37dff7e07b210d290956
Christopher Bilger <christopherbilg@gmail.com>
2020-07-17 21:46:45 -0400

    3d prism formulas added

--- src/3d/index.ts
+++ src/3d/index.ts
@@ -1,2 +1,3 @@
 export * from './sphere'
 export * from './cone'
+export * from './prism'
---
+++ src/3d/prism/prism.ts
@@ -0,0 +1,4 @@
+// Basic
+export const PrismSALateral = (lateralEdgeLength: number, sectionPerimeter: number): number => lateralEdgeLength * sectionPerimeter
+export const PrismVFromBaseArea = (height: number, areaOfBase: number): number => height * areaOfBase
+export const PrismVFromSectionArea = (lateralEdgeLength: number, areaOfSection: number): number => lateralEdgeLength * areaOfSection
